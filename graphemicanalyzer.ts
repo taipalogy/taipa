4462321384d6e54f1423a82d2044d78cfad91ed9
Wessen <sjhuaii@gmail.com>
2018-03-09 23:22:43 +0800

    hourly submission

--- graphemicanalyzer.ts
+++ graphemicanalyzer.ts
@@ -1,13 +1,24 @@
 import { Expression } from './expression';
+import { extend } from 'webdriver-js-extender';
 
 //------------------------------------------------------------------------------
 //  Regular Expressions
 //------------------------------------------------------------------------------
 
 export class GraphemicAnalyzerRegex {
-    public static readonly letter = /c|j|l|m|q|v/g;
-    public static readonly digraph = /ss|zs|xx|nn|ur|dr|or/g;
-    public static readonly trigraph = /zzs/g;
+    // alphabet
+    public static readonly singleGrapheme = /a|b|c|e|f|g|h|i|j|k|l|m|p|q|t|v|w|y/g;
+    // digraph: ss, zs, xx, ng, nn, ur, dr, or
+    public static readonly graphemeS = /s/g;
+    public static readonly graphemeX = /x/g;
+    public static readonly graphemeN = /n/g;
+    public static readonly graphemeG = /g/g;
+    public static readonly graphemeU = /u/g;
+    public static readonly graphemeR = /r/g;
+    public static readonly graphemeD = /d/g;
+    public static readonly graphemeO = /o/g;
+    // the sixth tone. trigraph: zzs
+    public static readonly graphemeZ = /z/g;
 }
 
 //------------------------------------------------------------------------------
@@ -22,6 +33,21 @@ export class Grapheme extends Expression {
     literal: string;
 }
 
+export class AlphabetGrapheme extends Expression {
+    graphemeOne: string;
+    // for digraph and trigraph
+    graphemeTwo: string;
+    // for trigraph
+    graphemeThree: string;
+
+    constructor() {
+        super();
+        this.graphemeOne = '';
+        this.graphemeTwo = '';
+        this.graphemeThree = '';
+    }
+}
+
 //------------------------------------------------------------------------------
 //  State pattern
 //------------------------------------------------------------------------------
@@ -34,8 +60,19 @@ class State implements StateLike {
     analyze(context: StateContext, literal: string) { return null; }
 }
 
+class GraphemeOneState extends State {
+    analyze(context: StateContext, literal: string) {
+        console.log("reached graphemeonestate. literal:%s", literal);
+        if(literal.search(GraphemicAnalyzerRegex.singleGrapheme) == 0) {
+        }
+    }
+}
+
 class LetterState extends State {
     analyze(context: StateContext, literal: string) {
+        context.graphemes.push(new AlphabetGrapheme());
+        context.setState(new GraphemeOneState());
+        context.analyze(literal);
     }
 }
 
@@ -66,5 +103,10 @@ class StateContext {
 //------------------------------------------------------------------------------
 
 export class GraphemicAnalyzer implements Expression {
+    sc: StateContext;
     literal: string;
+
+    constructor() {
+        this.sc = new StateContext();
+    }
 }
