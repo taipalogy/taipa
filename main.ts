d53231bf5678e13ccb87ae63b097183ea4dbd200
Wessen Sotron <sjhuaii@gmail.com>
2019-02-05 11:10:48 +0800

    move output routine from main to client

--- main.ts
+++ main.ts
@@ -9,54 +9,9 @@ const argc = process.argv.splice(2);
 if(argc.length == 1) {
     const input = argc[0];
 
-    let clt = new Client();
-    let doc = clt.processOneToken(input);
-    let output = ''
-    for(let i in doc.inputingLexemes) {
-        let l = doc.inputingLexemes[i].word.literal
-        let en = doc.inputingLexemes[i].getInflectionalEnding()
-        if(l.length-en.length != 0) {
-            output += l.substr(0, l.length-en.length) + ' - ' + 'inflectional stem'
-        }
-        let filler: string = ''
-        for(let n = 0; n < l.substr(0, l.length-en.length).length; n++) { 
-            filler += ' '
-        }
-        if(en.length > 0) output += '\n' + filler + en + ' - ' + 'inflectional ending'
+    let cli = new Client()
 
-        for(let j in doc.inputingMorphemes) {
-            let syll = ''
-            let saunz = []
-            for(let k in doc.inputingMorphemes[j]) {
-                let sou = doc.inputingMorphemes[j][k]
-                saunz.push('  - ' + sou.getLiteral() + ' - ' + sou.name)
-                syll += sou.getLiteral()
-            }
-            output += '\n' + '- ' + syll
-            for(let k in saunz) {
-                output += '\n' + saunz[k]
-            }
-        }
-
-        let ipw = clt.lookup(doc.inputingLexemes[i].word.literal);
-        // when the input word can be found in the dictionary
-        if(ipw != null) {
-            output += '\n' + ipw
-        }
-
-        let ls = doc.inputingLexemes[i].getBaseForms()
-
-        for(let j in ls) {
-            let bsw = clt.lookup(ls[j].literal);
-            // when the base form of the word can be found in the dictionary
-            if(bsw != null) {
-                output += '\n' + bsw
-            }
-        }
-
-    }
-
-    console.info(output)
+    console.info(cli.output(input))
 
     process.exit(1);
 } else if(argc.length > 1) {
--- morphrules.ts
+++ morphrules.ts
@@ -2,20 +2,20 @@
 
 export let MORPH_RULES = {
     'PRP': {
-        'guay':            {lemma: 'PRON_LEMMA', case: 'Acc'},
-        'gua':             {lemma: 'guay', case: ''},
-        'guazs':           {lemma: 'guay', case: ''},
-        'guaw':            {lemma: 'guay', case: ''},
+        'goay':            {lemma: 'PRON_LEMMA', case: 'Acc'},
+        'goa':             {lemma: 'guay', case: ''},
+        'goazs':           {lemma: 'guay', case: ''},
+        'goaw':            {lemma: 'guay', case: ''},
     },
 
     'VB': {
-        'hingzs':           {lemma: 'hingx', type: 'ditransitive'},
+        'hengzs':           {lemma: 'hingx', type: 'ditransitive'},
         'ziurhy':           {lemma: 'ziurh', type: 'ditransitive'},
         'how':              {lemma: 'hozs', type: 'ditransitive'},
         'sangy':            {lemma: 'sangw', type: 'ditransitive'},
         'mngw':             {lemma: 'mngzs', type: 'ditransitive'},
 
-        'uannw':            {lemma: 'uannzs', type: 'transitive'},
+        'oannw':            {lemma: 'uannzs', type: 'transitive'},
     },
 
     'PART': {
