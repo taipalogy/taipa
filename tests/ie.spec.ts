cb92da1d9ad8656178f695bea74d6ef2d7ad3f53
Wessen Sotron <sjhuaii@gmail.com>
2019-09-05 19:34:52 +0800

    add tests for lemma and inflectional ending

---
+++ tests/ie.spec.ts
@@ -0,0 +1,128 @@
+import { Client } from '../src/client'
+import { Document } from '../src/document'
+
+describe("Inflectional ending testing of gengzchiapf", () => {
+    let cli = new Client();
+    let doc = new Document();
+
+    doc = cli.processTonal('gengzchiapf');
+
+    test("check the inflectional stem", () => {
+        let l = doc.word.literal;
+        let en = doc.inflectionalEnding;
+        expect(l.substr(0, l.length-en.length)).toEqual('gengzchiap');
+    });
+  
+    test("check the inflectional ending", () => {
+        expect(doc.inflectionalEnding).toEqual('f');
+    });
+
+    test("check the sound of inflectional ending", () => {
+        expect(doc.soundSequences[1][4].getLiteral()).toEqual('f');
+    });
+});
+
+describe("Inflectional ending testing of piauzpietf", () => {
+    let cli = new Client();
+    let doc = new Document();
+
+    doc = cli.processTonal('piauzpietf');
+
+    test("check the inflectional stem", () => {
+        let l = doc.word.literal;
+        let en = doc.inflectionalEnding;
+        expect(l.substr(0, l.length-en.length)).toEqual('piauzpiet');
+    });
+  
+    test("check the inflectional ending", () => {
+        expect(doc.inflectionalEnding).toEqual('f');
+    });
+
+    test("check the sound of inflectional ending", () => {
+        expect(doc.soundSequences[1][4].getLiteral()).toEqual('f');
+    });
+});
+
+describe("Inflectional ending testing of tengzsekf", () => {
+    let cli = new Client();
+    let doc = new Document();
+
+    doc = cli.processTonal('tengzsekf');
+
+    test("check the inflectional stem", () => {
+        let l = doc.word.literal;
+        let en = doc.inflectionalEnding;
+        expect(l.substr(0, l.length-en.length)).toEqual('tengzsek');
+    });
+  
+    test("check the inflectional ending", () => {
+        expect(doc.inflectionalEnding).toEqual('f');
+    });
+
+    test("check the sound of inflectional ending", () => {
+        expect(doc.soundSequences[1][3].getLiteral()).toEqual('f');
+    });
+});
+
+describe("Inflectional ending testing of sekfhappw", () => {
+    let cli = new Client();
+    let doc = new Document();
+
+    doc = cli.processTonal('sekfhappw');
+
+    test("check the inflectional stem", () => {
+        let l = doc.word.literal;
+        let en = doc.inflectionalEnding;
+        expect(l.substr(0, l.length-en.length)).toEqual('sekfhapp');
+    });
+  
+    test("check the inflectional ending", () => {
+        expect(doc.inflectionalEnding).toEqual('w');
+    });
+
+    test("check the sound of inflectional ending", () => {
+        expect(doc.soundSequences[1][3].getLiteral()).toEqual('w');
+    });
+});
+
+describe("Inflectional ending testing of kakfsittw", () => {
+    let cli = new Client();
+    let doc = new Document();
+
+    doc = cli.processTonal('kakfsittw');
+
+    test("check the inflectional stem", () => {
+        let l = doc.word.literal;
+        let en = doc.inflectionalEnding;
+        expect(l.substr(0, l.length-en.length)).toEqual('kakfsitt');
+    });
+  
+    test("check the inflectional ending", () => {
+        expect(doc.inflectionalEnding).toEqual('w');
+    });
+
+    test("check the sound of inflectional ending", () => {
+        expect(doc.soundSequences[1][3].getLiteral()).toEqual('w');
+    });
+});
+
+describe("Inflectional ending testing of qeysiokkw", () => {
+    let cli = new Client();
+    let doc = new Document();
+
+    doc = cli.processTonal('qeysiokkw');
+
+    test("check the inflectional stem", () => {
+        let l = doc.word.literal;
+        let en = doc.inflectionalEnding;
+        expect(l.substr(0, l.length-en.length)).toEqual('qeysiokk');
+    });
+  
+    test("check the inflectional ending", () => {
+        expect(doc.inflectionalEnding).toEqual('w');
+    });
+
+    test("check the sound of inflectional ending", () => {
+        expect(doc.soundSequences[1][4].getLiteral()).toEqual('w');
+    });
+});
---
+++ tests/lemma.spec.ts
@@ -0,0 +1,102 @@
+import { Client } from '../src/client'
+import { Document } from '../src/document'
+
+describe("Lemma testing of chitt", () => {
+    let cli = new Client()
+    let doc = new Document()
+
+    doc = cli.processTonal('chitt')
+
+    test("check the number of lemmata", () => {
+      expect(doc.lemmata.length).toEqual(0);
+    });
+});
+
+describe("Lemma testing of suzjippwhoatf", () => {
+  let cli = new Client()
+  let doc = new Document()
+
+  doc = cli.processTonal('suzjippwhoatf');
+
+  test("check the number of lemmata", () => {
+    expect(doc.lemmata.length).toEqual(1);
+  });
+
+  test("check the lemma", () => {
+    expect(doc.lemmata[0].literal).toEqual('suzjippwhoat');
+  });
+});
+
+describe("Lemma testing of sia", () => {
+  let cli = new Client()
+  let doc = new Document()
+
+  doc = cli.processTonal('sia');
+
+  test("check the number of lemmata", () => {
+    expect(doc.lemmata.length).toEqual(1);
+  });
+
+  test("check the lemma", () => {
+    expect(doc.lemmata[0].literal).toEqual('siay');
+  });
+});
+
+describe("Lemma testing of siay", () => {
+  let cli = new Client()
+  let doc = new Document()
+
+  doc = cli.processTonal('siay');
+
+  test("check the number of lemmata", () => {
+    expect(doc.lemmata.length).toEqual(1);
+  });
+
+  test("check the lemma", () => {
+    expect(doc.lemmata[0].literal).toEqual('siaw');
+  });
+});
+
+describe("Lemma testing of siaw", () => {
+  let cli = new Client()
+  let doc = new Document()
+
+  doc = cli.processTonal('siaw');
+
+  test("check the number of lemmata", () => {
+    expect(doc.lemmata.length).toEqual(2);
+  });
+
+  test("check the lemma", () => {
+    expect(doc.lemmata[0].literal).toEqual('siaz');
+    expect(doc.lemmata[1].literal).toEqual('siax');
+  });
+});
+
+describe("Lemma testing of siaz", () => {
+  let cli = new Client()
+  let doc = new Document()
+
+  doc = cli.processTonal('siaz');
+
+  test("check the number of lemmata", () => {
+    expect(doc.lemmata.length).toEqual(3);
+  });
+
+  test("check the lemma", () => {
+    expect(doc.lemmata[0].literal).toEqual('siax');
+    expect(doc.lemmata[1].literal).toEqual('siacf');
+    expect(doc.lemmata[2].literal).toEqual('sia');
+  });
+});
+
+describe("Lemma testing of siax", () => {
+  let cli = new Client()
+  let doc = new Document()
+
+  doc = cli.processTonal('siax');
+
+  test("check the number of lemmata", () => {
+    expect(doc.lemmata.length).toEqual(0);
+  });
+});
