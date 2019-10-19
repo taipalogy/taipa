ae2c61e7d544b5e3bacb86e1e0cc748ee73f3788
Wessen Sotron <sjhuaii@gmail.com>
2019-10-19 15:44:22 +0800

    add npxbin.ts in scripts folder

--- bin/taipa.js
+++ bin/taipa.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("../lib/client");
-const document_1 = require("../lib/document");
+const client_1 = require("../lib/client")
+const document_1 = require("../lib/document")
 let cli = new client_1.Client();
 let doc = new document_1.Document();
 let stdin = process.openStdin();
-stdin.addListener("data", function (d) {
+stdin.addListener('data', function (d) {
     doc = cli.process(d.toString().trim());
     let rs = doc.relations;
     if (rs.length > 0) {
@@ -15,3 +15,4 @@ stdin.addListener("data", function (d) {
         }
     }
 });
+//# sourceMappingURL=taipa.js.map
