c5072558df9059c0db78fac118a6ea343e996da9
Wessen Sotron <sjhuaii@gmail.com>
2020-01-07 00:10:28 +0800

    add spelling.ts

---
+++ src/tonal/spelling.ts
@@ -0,0 +1,15 @@
+import { Tonal } from "./version2";
+
+interface Tonals {
+    tonals: Array<Tonal>;
+}
+
+export class TwoTonals implements Tonals {
+    tonals: [Tonal, Tonal] = [new Tonal(), new Tonal()];
+    constructor(t1: Tonal, t2: Tonal) {
+        this.tonals[0] = t1;
+        this.tonals[1] = t2;
+    }
+}
+
+export type TwoTonalSpelling = { pos: [number, number], patterns: TwoTonals[] };
